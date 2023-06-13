6f304a5dc4e3f41e1d8b32ce005fd242c3612bea
Maria <mas04081997@mail.ru>
2023-06-13 13:46:09 +0300

    Дз 10\13\15

---
+++ Дз.Задача13/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей
+//цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+ while (number > 999)
+    number = number / 10;
+    { 
+       if(number < 100)
+       {
+        Console.Write("Третий цифры нет");
+       }
+       else
+       {
+        int result = number % 10;
+        Console.WriteLine(result);
+       }
+    }
