f7bddddb7b3708b41279bb63d95366612bd5fb70
Maria <mas04081997@mail.ru>
2023-06-13 12:27:01 +0300

    add Test006/007/008/009/010

---
+++ Test007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа.
+
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+int fistnumber = number / 100;
+int thirdnumber = number % 10;
+
+//Console.WriteLine(number +" " + fistnumber + thirdnumber);
+Console.WriteLine(fistnumber * 10 + thirdnumber + " " + number);
---
+++ Test008/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли второе число
+//кратным первому. Если число 2 не кратно числу 1, то
+//программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+
+Console.WriteLine("Ввести первое число");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Ввести второе число");
+int numberB = int.Parse(Console.ReadLine()!);
+
+int result = numberA % numberB;
+
+if (result == 0)
+{
+    Console.WriteLine(numberA + "кратно"+ numberB);
+}
+else
+{
+    Console.WriteLine(numberA + " не кратно, остаток" +" " + result);
+}
