ecbc0ffa9acf183d51c0a5ab7ca13df3487a4ccc
Maria <mas04081997@mail.ru>
2023-06-16 10:32:59 +0300

    add

--- Дз.Задача13/Program.cs
+++ Дз.Задача13/Program.cs
@@ -6,15 +6,58 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
+//string mess = "Введите число: ";
+//int number1 = Prompt(mess);
+//
+//int result = FindThirdNumber(number1);
+//if (result == -1)
+//{
+//    Console.WriteLine("Такой цифры нет! ");
+//}
+//else
+//{
+//    Console.WriteLine($"Третья цифра числа {result}");
+//}
+//
+//
+//
+//int Prompt(string message)
+//{
+//    Console.Clear();
+//    Console.WriteLine(message);
+//    int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//    return num;
+//}
+//
+//int FindThirdNumber(int num)
+//{
+//    if (num < 100)
+//    {
+//        return -1;
+//
+//    }
+//    else
+//    {
+//        while (num >= 1000)
+//        {
+//            num = num / 10;
+//        }
+//        int thirdNumber = num % 10;
+//        return thirdNumber;
+//    }
+//}
+
+
+
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine()!);
 
  while (number > 999)
     number = number / 10;
-    { 
+        { 
        if(number < 100)
        {
-        Console.Write("Третий цифры нет");
+        Console.Write ("Третий цифры нет");
        }
        else
        {
@@ -22,3 +65,7 @@ int number = int.Parse(Console.ReadLine()!);
         Console.WriteLine(result);
        }
     }
+
+
+
+
