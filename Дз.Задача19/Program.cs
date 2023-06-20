6fc54055b9893846d8f7d3a311fd416b12ff5c74
Maria <mas04081997@mail.ru>
2023-06-20 09:32:24 +0300

    add fils дз.Задача 19/21/23

--- Дз.Задача19/Program.cs
+++ Дз.Задача19/Program.cs
@@ -5,3 +5,23 @@
 //14212 -> нет
 //23432 -> да
 //12821 -> да
+
+Console.Write("Введите пятизначное число ");
+int num = int.Parse(Console.ReadLine());
+NumberCheck(num);
+
+void NumberCheck(int num)
+{
+    int num1 = num / 10000;
+    int num2 = num % 10000 / 1000;
+    int num4 = num % 100 / 10;
+    int num5 = num % 10;
+
+    if(num >= 10000 && num < 100000)
+    {
+        if(num1 == num5 && num2 == num4)
+        Console.WriteLine($"Число {num} является палиндромом");
+        else Console.WriteLine($"Число {num} не является палиндромом");
+    }
+    else Console.WriteLine($"Число  {num} не является пятизначным");
+}
--- Дз.Задача23/Program.cs
+++ Дз.Задача23/Program.cs
@@ -5,3 +5,53 @@
 
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+int N = InputInt();
+
+int[] cubesTable = GetTableOfCubes(N);
+
+PrintArray(cubesTable);
+
+
+
+
+
+
+int InputInt()
+{
+    Console.WriteLine("Введите целое число");
+    bool isN = int.TryParse(Console.ReadLine(), out int N);
+    if (isN)
+    {
+        return N;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+
+
+int [] GetTableOfCubes(int N)
+{
+    int[] arr = new int[N];
+    int i = 0;
+    while (i < N)
+    {
+        arr[i] = (int)(Math.Pow(i+1, 3));
+        i++;                         
+    }
+    return arr;
+}
+
+
+void PrintArray (int[] arr)
+{
+       int i = 0;
+    while (i < N)
+    {
+        Console.Write($"{arr[i]} ");
+        i++;
+    }
+}
